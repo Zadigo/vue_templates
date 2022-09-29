bbe21272ba0c26faec9070b006ff4d897436f36b
Zadigo <pendenquejohn@gmail.com>
2022-09-29 17:17:12 +0200

    Modal + Offcanva

--- src/layouts/BaseModal.vue
+++ src/layouts/BaseModal.vue
@@ -1,27 +1,26 @@
 <template>
-  <div class="modal-wrapper">
-    <transition appear name="custom-classes" mode="out-in" enter-active-class="modal-animation dialog-fade-in-down" leave-active-class="modal-animation dialog-fade-out-up">
-      <!-- <transition name="slide" mode="out-in"> -->
-      <div v-if="show" :id="id" :class="modalClasses" :aria-modal="show" class="modal" role="dialog" tabindex="-1">
-        <div :class="modalDialogClasses" class="modal-dialog">
-          <div :class="modalContentClasses" class="modal-content">
-            <div v-if="!isFrame" class="modal-header">
-              <h5 v-if="title" class="modal-title">
-                {{ title }}
-              </h5>
-              <button type="button" class="btn-close" aria-label="Close" @click="$emit('close')"></button>
-            </div>
-
-            <div class="modal-body">
-              Google
-              <slot></slot>
-            </div>
-
-            <slot name="footer"></slot>
+  <div ref="link" class="modal-wrapper">
+    <!-- <transition appear name="custom-classes" mode="out-in" enter-active-class="modal-animation dialog-fade-in-down" leave-active-class="modal-animation dialog-fade-out-up"> -->
+    <!-- <transition name="slide" mode="out-in"> -->
+    <div v-if="show" :id="id" :class="modalClasses" :aria-modal="show" class="modal" role="dialog" tabindex="-1">
+      <div :class="modalDialogClasses" class="modal-dialog">
+        <div :class="modalContentClasses" class="modal-content">
+          <div v-if="!isFrame" class="modal-header">
+            <h5 v-if="title" class="modal-title">
+              {{ title }}
+            </h5>
+            <button type="button" class="btn-close" aria-label="Close" @click="$emit('close')"></button>
           </div>
+
+          <div class="modal-body">
+            <slot></slot>
+          </div>
+
+          <slot name="footer"></slot>
         </div>
       </div>
-    </transition>
+    </div>
+    <!-- </transition> -->
 
     <transition appear name="custom-classes" mode="out-in" enter-active-class="modal-animation dialog-fade-in" leave-active-class="modal-animation dialog-fade-out">
       <div v-if="show" :class="{show}" class="modal-backdrop"></div>
@@ -92,6 +91,7 @@ export default {
           break
       }
       return [
+        // 'fade',
         this.show ? 'show' : null,
         position
       ]
@@ -139,8 +139,14 @@ export default {
     }
   },
   watch: {
-    show () {
-      this.toggleBody()
+    show (current) {
+      this.toggleHtmlBody()
+
+      if (current) {
+        this.$refs.link.scrollTop
+      } else {
+        this.reflow()
+      }
     }
   },
   mounted () {
@@ -152,15 +158,15 @@ export default {
     body.removeEventListener('click', this.backdropClick)
   },
   methods: {
-    toggleBody () {
+    toggleHtmlBody () {
       const body = this.getBody()
 
       if (body.classList.contains('modal-open')) {
         body.classList.remove('modal-open')
-        body.style.overflow = null
+        // body.style.overflow = null
       } else {
         body.classList.add('modal-open')
-        body.style.overflow = 'hidden'
+        // body.style.overflow = 'hidden'
       }
     },
     getBody () {
@@ -179,6 +185,9 @@ export default {
         }
       }
     },
+    reflow () {
+      this.$refs.link.offsetHeight
+    }
   }
 }
 </script>
@@ -193,25 +202,7 @@ export default {
   opacity: .5;
 }
 
-/* .slide-enter-active,
-.slide-leave-active {
-  animation-duration: 0.5s;
-  animation-fill-mode: both;
-}
-
-.slide-enter-from,
-.slide-leave-to {
-  opacity: 0;
-  transform: translate3d(0, -50%, 0);
-}
-
-.slide-enter-to,
-.slide-leave-from {
-  opacity: 1;
-  transform: translate3d(0, 0, 0);
-} */
-
-.modal-animation {
+/* .modal-animation {
   animation-duration: 0.5s;
   animation-fill-mode: both;
 }
@@ -273,5 +264,5 @@ export default {
   to {
     opacity: 0;
   }
-}
+} */
 </style>
