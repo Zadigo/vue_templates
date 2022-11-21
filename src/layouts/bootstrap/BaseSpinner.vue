769053298b349f0086fe6e7798815598981f171a
Zadigo <pendenquejohn@gmail.com>
2022-11-21 22:10:02 +0100

    BaseSpinner

---
+++ src/layouts/bootstrap/BaseSpinner.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- aria-hidden="true" -->
+  <div :class="spinnerClasses" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseSpinner',
+  props: {
+    color: {
+      type: String,
+      default: 'light'
+    },
+    grow: {
+      type: Boolean
+    },
+    size: {
+      type: String,
+      default: 'md'
+    }
+  },
+  computed: {
+    spinnerClasses () {
+      return [
+        {
+          'spinner-border': !this.grow,
+          'spinner-grow': this.grow,
+          [`spinner-border-${this.size}`]: !this.grow,
+          [`spinner-grow-${this.size}`]: this.grow,
+          [`text-${this.color}`]: true
+        }
+      ]
+    }
+  }
+}
+</script>
