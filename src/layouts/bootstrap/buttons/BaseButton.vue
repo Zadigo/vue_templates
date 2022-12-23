a4a708e46c0ee6fc5527c5022b3bc86ed971ef03
Zadigo <pendenquejohn@gmail.com>
2022-12-23 15:36:30 +0100

    Create buttons components

--- src/layouts/bootstrap/buttons/BaseButton.vue
+++ src/layouts/bootstrap/buttons/BaseButton.vue
@@ -1,28 +1,32 @@
 <template>
-  <router-link v-if="to" :to="to" :class="buttonClasses">
+  <router-link v-if="to" :id="computedId" :to="to" :class="buttonClasses" v-bind="buttonAttrs">
     <slot></slot>
   </router-link>
   
-  <button v-else :class="buttonClasses" type="button">
+  <button v-else :id="computedId" :class="buttonClasses" v-bind="buttonAttrs" type="button">
     <slot></slot>
   </button>
 </template>
 
 <script>
 import { ref } from 'vue'
+import { useUtilities } from '../composables'
 
 export default {
   name: 'BaseButton',
   props: {
-    placeholder: {
-      type: Boolean
-    },
-    disabled: {
-      type: Boolean
+    id: {
+      type: String
     },
     color: {
       type: String,
       default: 'primary'
+    },
+    disabled: {
+      type: Boolean
+    },
+    placeholder: {
+      type: Boolean
     },
     to: {
       type: [String, Object]
@@ -30,8 +34,10 @@ export default {
   },
   setup () {
     const target = ref(null)
+    const { generateId } = useUtilities()
     return {
-      target
+      target,
+      generateId
     }
   },
   computed: {
@@ -44,6 +50,14 @@ export default {
           'placeholder': this.placeholder 
         }
       ]
+    },
+    buttonAttrs () {
+      return {
+
+      }
+    },
+    computedId () {
+      return this.id || this.generateId('btn')
     }
   }
 }
---
+++ src/layouts/bootstrap/buttons/BaseDropdownButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dropdown">
+    <base-button :id="id" :aria-expanded="show" class="dropdown-toggle" type="button" @click="show = !show">
+      Drop
+    </base-button>
+    
+    <!-- <base-drop-menu :show="show" @click:dropdown-item="show = !show" /> -->
+    <slot :show="show" @click:dropdown-item="handleClick"></slot>
+  </div>
+</template>
+
+<script>
+import { useClickOutside } from '../composables'
+
+import BaseButton from './BaseButton.vue'
+
+export default {
+  name: 'BaseDropdownButton',
+  components: {
+    BaseButton
+  },
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  setup () {
+    const { show, handleClick } = useClickOutside()
+    return {
+      show,
+      handleClick
+    }
+  }
+}
+</script>
