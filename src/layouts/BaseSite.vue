746f0d07483414a7dd576e48b1cb7ee07cd0fda2
Zadigo <pendenquejohn@gmail.com>
2023-04-27 16:31:47 +0200

    Base site structure

---
+++ src/layouts/BaseSite.vue
@@ -0,0 +1,100 @@
+<template>
+  <section class="hero">
+    <!-- Navbar -->
+    <base-navbar :fixed-top="true" nav-brand="Vue Templates">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <base-mega-dropdown :items="megamenu" />
+        <base-side-dropdown link-name="Templates" />
+        <nav-item href="#hero-features" link-name="Hero templates" />
+      </ul>
+
+      <div class="d-flex align-items-center">
+        <router-link :to="{ name: 'login_view' }" class="btn btn-link px-3 me-2">Login</router-link>
+        <router-link :to="{ name: 'signup_view' }" class="btn btn-primary me-3 btn-rounded">Signup</router-link>
+      </div>
+    </base-navbar>
+
+    <!-- Header -->
+    <slot name="header"></slot>
+
+    <!-- Body -->
+    <slot></slot>
+
+    <!-- Footer -->
+    <base-footer class="mt-3" :items="footer" brand="Vue Templates" />
+  </section>
+</template>
+
+<script>
+import { useDarkMode } from '../composables/darkmode'
+import { useDark, useToggle } from '@vueuse/core'
+import { provide } from 'vue'
+
+import megamenu from '../data/megamenu.json'
+import navitems from '../data/navitems.json'
+import footer from '../data/footer.json'
+
+import BaseMegaDropdown from '../layouts/bootstrap/nav/BaseMegaDropdown.vue'
+import BaseSideDropdown from '../layouts/bootstrap/nav/BaseSideDropdown.vue'
+import BaseFooter from '@/layouts/BaseFooter.vue'
+import BaseNavbar from '@/layouts/bootstrap/nav/BaseNavbar.vue'
+import NavItem from '@/layouts/bootstrap/nav/NavItem.vue'
+
+export default {
+  name: 'BaseSite',
+  components: {
+    BaseFooter,
+    BaseMegaDropdown,
+    BaseSideDropdown,
+    BaseNavbar,
+    NavItem
+  },
+  setup () {
+    var dark = useDark()
+    var toggleDark = useToggle(dark)
+
+    var { darkMode, toggleDarkMode } = useDarkMode()
+    provide('darkMode', darkMode)
+    return {
+      footer,
+      d: dark,
+      toggleDark,
+      darkMode,
+      toggleDarkMode,
+      navitems,
+      megamenu
+    }
+  },
+  data () {
+    return {
+      heroSettings: {
+        image: 12,
+        type: 'center'
+      },
+      drops: [
+        {
+          name: 'First',
+          to: null
+        },
+        {
+          name: 'second',
+          to: null
+        },
+        {
+          name: 'Header',
+          header: true
+        },
+        {
+          name: 'Third'
+        },
+        {
+          divider: true
+        },
+        {
+          name: 'Fourth'
+        }
+      ]
+    }
+  }
+}
+</script>
