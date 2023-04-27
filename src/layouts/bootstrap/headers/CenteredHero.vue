09fcfa72de70153eae13d9114bf21daca4924ed5
Zadigo <pendenquejohn@gmail.com>
2023-04-27 16:29:30 +0200

    Add bootstrap headers

---
+++ src/layouts/bootstrap/headers/CenteredHero.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="px-4 py-5 my-5 text-center shadow-sm">
+    <img class="d-block mx-auto mb-4" src="" alt="" width="72" height="57">
+    <h1 class="display-5 fw-bold text-body-emphasis">{{ lead }}</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">{{ leadDescription }}</p>
+      <slot name="cta">
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <base-button class="px-4 gap-3" size="lg">Primary</base-button>
+          <base-button class="px-4" size="lg" outline>Secondary</base-button>
+        </div>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseButton from '../buttons/BaseButton.vue'
+
+export default {
+  name: 'CenteredHero',
+  components: {
+    BaseButton
+  },
+  props: {
+    lead: {
+      type: String,
+      default: "Centered hero"
+    },
+    leadDescription: {
+      type: String,
+      default: "Quickly design and customize responsive mobile-first sites with Bootstrap, the world's most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins."
+    }
+  }
+}
+</script>
---
+++ src/layouts/bootstrap/headers/WithForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="shadow-sm">
+    <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+      <div class="row align-items-center g-lg-5 py-5">
+        <div class="col-lg-7 text-center text-lg-start">
+          <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">Vertically centered hero sign-up form</h1>
+          <p class="col-lg-10 fs-4">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+        </div>
+
+        <div class="col-md-10 mx-auto col-lg-5">
+          <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+            <slot name="inputs">
+              <base-input id="name" placeholder="Nom" class="p-3 mb-2" />
+              <base-input id="name" input-type="email" placeholder="Email" class="p-3 mb-3" />
+            </slot>
+            
+            <base-checkbox id="remember-me" label="Remember me" class="mb-3" />
+
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+            <hr class="my-4">
+            <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import BaseCheckbox from '../BaseCheckbox.vue'
+import BaseInput from '@/layouts/bootstrap/BaseInput.vue'
+export default {
+  name: 'WithForm',
+  components: {
+    BaseCheckbox,
+    BaseInput
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
