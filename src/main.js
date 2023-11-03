3c907c81427452353ebf723a6cf487623124459d
Zadigo <pendenquejohn@gmail.com>
2023-11-03 23:07:55 +0100

    Update package

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { loadFonts } from './plugins'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { createVueLocalStorage, createVueSession } from './plugins/vue-storages'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
 
 import App from './App.vue'
 
+import 'vuetify/styles'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'mdb-ui-kit/css/mdb.min.css'
 import '@mdi/font/css/materialdesignicons.css'
@@ -13,8 +15,11 @@ import './plugins/fontawesome'
 import './assets/style.scss'
 import './layouts/bootstrap/css/style.css'
 
-import router from './router'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
+import router from './router'
 
 loadFonts()
 
@@ -23,11 +28,24 @@ const app = createApp(App)
 const pinia = createPinia()
 const session = createVueSession()
 const localstorage = createVueLocalStorage()
+const vuetify = createVuetify({ 
+  components, 
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
+})
+
 
 app.use(pinia)
 app.use(router)
 app.use(session)
 app.use(localstorage)
+app.use(vuetify)
 app.component('FontAwesomeIcon', FontAwesomeIcon)
 
 app.mount('#app')
